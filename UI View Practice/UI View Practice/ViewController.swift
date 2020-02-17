b1e0b736afbbfd7d93e07c3babae5117d2f08ca0
Benjamín Valdez <benjamin.vr.1998@gmail.com>
2020-02-17 09:25:16 -0600

    Added a function that moves an image using a slider

--- UI View Practice/UI View Practice/ViewController.swift
+++ UI View Practice/UI View Practice/ViewController.swift
@@ -8,7 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var imgBall: UIImageView!
+    @IBOutlet weak var viTopView: UIView!
+    
+    @IBAction func slBar(_ sender: UISlider) {
+        let currentValue = CGFloat(sender.value)
+        let xPosition = currentValue * viTopView.frame.width
+        
+        imgBall.frame = CGRect(x: xPosition, y: imgBall.frame.origin.y, width: imgBall.frame.size.width, height: imgBall.frame.size.height)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
