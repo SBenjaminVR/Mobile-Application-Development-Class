c6b130912101a163e080c27b6fe081e8f1ac70fc
Benjamín Valdez <benjamin.vr.1998@gmail.com>
2020-02-17 09:53:38 -0600

    Added an animation in the second UIView

--- UI View Practice/UI View Practice/ViewController.swift
+++ UI View Practice/UI View Practice/ViewController.swift
@@ -11,6 +11,8 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var imgBall: UIImageView!
     @IBOutlet weak var viTopView: UIView!
+    @IBOutlet weak var tfRana: UILabel!
+    @IBOutlet weak var viBottomView: UIView!
     
     @IBAction func slBar(_ sender: UISlider) {
         let currentValue = sender.value
@@ -19,6 +21,22 @@ class ViewController: UIViewController {
         imgBall.frame = CGRect(x: xPosition, y: imgBall.frame.origin.y, width: imgBall.frame.size.width, height: imgBall.frame.size.height)
     }
     
+    @IBAction func animarLetrero(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            UIView.animate(withDuration: 1, animations: {
+                self.tfRana.frame.origin.x = 50
+            })
+        case 1:
+            UIView.animate(withDuration: 1, animations: {
+                self.tfRana.frame.origin.x = 300
+            })
+        default:
+            break
+        }
+        
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
