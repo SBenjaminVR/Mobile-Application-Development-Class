87b75b7a8731e78b96610ac845a03a23d1429fec
Benjamín Valdez <benjamin.vr.1998@gmail.com>
2020-02-17 08:56:21 -0600

    Added a new project

---
+++ UI View Practice/UI View Practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UI View Practice
+//
+//  Created by Fabiola Valdez on 2/17/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
