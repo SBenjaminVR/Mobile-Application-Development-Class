f8153fc4255200ac7ccf59dbaa556a4ed1fdd62b
Benjamín Valdez <benjamin.vr.1998@gmail.com>
2020-02-20 09:04:35 -0600

    Added the initiainitial files for AutoLayout exercise

---
+++ AutoLayout Exercise/AutoLayout Exercise/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutoLayout Exercise
+//
+//  Created by Fabiola Valdez on 2/20/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
