1b339c77dbadf0a89e87e3cc8ed771282c18b913
Benjamín Valdez <benjamin.vr.1998@gmail.com>
2020-03-02 08:48:12 -0600

    Added tableView practice

---
+++ tableView/tableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tableView
+//
+//  Created by Fabiola Valdez on 3/2/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
