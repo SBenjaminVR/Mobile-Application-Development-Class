17965499fe47797bdc6b82e98bc8bab3dab4de8b
Benjamín Valdez <benjamin.vr.1998@gmail.com>
2020-03-12 09:51:15 -0600

    finished plist practice

--- cargarDatosPList/cargarDatosPList/ViewController.swift
+++ cargarDatosPList/cargarDatosPList/ViewController.swift
@@ -7,13 +7,52 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     var arrDiccionarios: NSArray!
 
+    @IBOutlet weak var lbCapacidad: UILabel!
+    @IBOutlet weak var lbPrecio: UILabel!
+    @IBOutlet weak var lbTipo: UILabel!
+    @IBOutlet weak var imgFoto: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let path = Bundle.main.path(forResource: "Property List", ofType: "plist")
+        arrDiccionarios = NSArray(contentsOfFile: path!)
+    }
+    
+    //MARK: - Metodos de Table View
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return arrDiccionarios.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let celda = tableView.dequeueReusableCell(withIdentifier: "celda")!
+        let dic = arrDiccionarios[indexPath.row] as! NSDictionary
+        
+        celda.textLabel?.text = dic["tipo"] as? String
+        
+        return celda
+    }
+    
+    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        let dic = arrDiccionarios[indexPath.row] as! NSDictionary
+        lbTipo.text = dic["tipo"] as? String
+        lbCapacidad.text = dic["tipo"] as? String
+       
+        let NSNum = dic["precio"] as! NSNumber
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        //formatter.currencyCode = "USD"
+        formatter.currencySymbol = "$"
+        lbPrecio.text = formatter.string(from: NSNum)
+        
+        let sUrl = dic["urlFoto"] as? String
+        let url = URL(string: sUrl!)
+        let imgData = NSData(contentsOf: url!)
+        imgFoto.image = UIImage(data: imgData! as Data)
+        
     }
 
 
