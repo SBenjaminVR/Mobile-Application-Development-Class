a30363d68df902efba10368ae719144ffdb64973
Benjamín Valdez <benjamin.vr.1998@gmail.com>
2020-03-12 09:07:05 -0600

    Started a Plist practice

---
+++ cargarDatosPList/cargarDatosPList/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  cargarDatosPList
+//
+//  Created by Benjamin Valdez on 3/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var arrDiccionarios: NSArray!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
